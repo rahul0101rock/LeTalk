380be49526ee0e11163c8f2bd2a16ff4d0cbad29
Rahul Choudhary <rahul0101rock@gmail.com>
2021-06-10 06:59:57 +0000

    Name changing and configuring

---
+++ chat/views.py
@@ -0,0 +1,9 @@
+
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+def index(request):
+    template = loader.get_template('chat/index.html')
+    return HttpResponse(template.render())
