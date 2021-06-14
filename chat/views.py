7228b333c5b0be26daf722fdf7a72f9ef579f06a
Rahul Choudhary <rahul0101rock@gmail.com>
2021-06-14 06:05:02 +0000

    img root and css fixed

--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,11 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.db import models
+
+f = FileSystemStorage(location='/media')
+
 def index(request):
     template = loader.get_template('chat/index.html')
     return HttpResponse(template.render())
