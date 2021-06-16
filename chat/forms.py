7b86a44d1a5fb6240cbdee44177eb7e4ae34bea9
Rahul Choudhary <rahul0101rock@gmail.com>
2021-06-16 17:30:29 +0000

    configure user authentication

---
+++ chat/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control field', 'placeholder':'Email Address'}), )
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2',)
+
+	def __init__(self, *args, **kwargs):
+	    super(SignUpForm, self).__init__(*args, **kwargs)
+
+	    self.fields['username'].widget.attrs['class'] = 'form-control field'
+	    self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+	    self.fields['username'].label = ''
+	    self.fields['username'].help_text = ''
+
+	    self.fields['password1'].widget.attrs['class'] = 'form-control field'
+	    self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+	    self.fields['password1'].label = ''
+	    self.fields['password1'].help_text = ''
+
+	    self.fields['password2'].widget.attrs['class'] = 'form-control field'
+	    self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+	    self.fields['password2'].label = ''
+	    self.fields['password2'].help_text = ''
--- chat/views.py
+++ chat/views.py
@@ -3,10 +3,12 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.core.files.storage import FileSystemStorage
 from django.db import models
-
+from .forms import SignUpForm
 f = FileSystemStorage(location='/media')
 
 def index(request):
@@ -17,9 +19,23 @@ def login(request):
     template = loader.get_template('chat/login.html')
     return HttpResponse(template.render())
 
+
 def register(request):
-    template = loader.get_template('chat/register.html')
-    return HttpResponse(template.render())
+	if request.method =='POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request, ('Youre now registered'))
+			return redirect('home')
+	else:
+		form = SignUpForm()
+
+	context = {'form': form}
+	return render(request, 'chat/register.html', context)
 
 def profile(request):
     template = loader.get_template('chat/profile.html')
