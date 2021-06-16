ef92dc8b1d119b3912ea744447d18f921930593a
Rahul Choudhary <rahul0101rock@gmail.com>
2021-06-16 18:20:27 +0000

    configure Login authentication

--- chat/views.py
+++ chat/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.core.files.storage import FileSystemStorage
 from django.db import models
@@ -17,10 +18,15 @@ def index(request):
     return HttpResponse(template.render())
 
 def login(request):
-    template = loader.get_template('chat/login.html')
-    return HttpResponse(template.render())
-
-
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            return redirect('/')
+        else:return redirect('login')
+    else:
+        return render(request, 'chat/login.html', {})
 def register(request):
 	if request.method =='POST':
 		form = SignUpForm(request.POST)
@@ -29,14 +35,11 @@ def register(request):
 			username = form.cleaned_data['username']
 			password = form.cleaned_data['password1']
 			user = authenticate(username=username, password=password)
-			#login(request,user)
-			messages.success(request, ('User registered'))
+			login(request)
 			return redirect('/')
 	else:
 		form = SignUpForm()
-
-	context = {'form': form}
-	return render(request, 'chat/register.html', context)
+	return render(request, 'chat/register.html', {'form': form})
 
 def profile(request):
     template = loader.get_template('chat/profile.html')
