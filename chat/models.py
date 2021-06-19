0d5748d6c0a2ecbc31c255272f86c2c426d3ca78
Rahul Choudhary <rahul0101rock@gmail.com>
2021-06-19 17:42:25 +0000

    added friend_list and request_list model. And profile is in progress

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,36 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 class user_search(models.Model):
     username=models.CharField(max_length=100)
+
+class friends_list(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="user")
+    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True,related_name="friends")
+
+    def add_in_list(self,acc):
+        if not acc in self.friends.all(): self.friends.add(acc)
+        self.save()
+
+    def remove_from_list(self,acc):
+        if acc in self.friends.all(): self.friends.remove(acc)
+        self.save()
+
+class request_list(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="receiver")
+    is_active=  models.BooleanField(blank=False, null=False, default=True)
+
+
+    def allow(self):
+        rl = friends_list.objects.get(user=self.recevier)
+        if rl:
+            rl.add_in_list(self.sender)
+            self.is_in = False
+            self.save()
+    def remove(self):
+        self.is_in = False
+        self.save()
+
+
